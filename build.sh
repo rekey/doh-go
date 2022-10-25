484aff1e2945dc6f9a8b419d5d0a3e15eb27888b
Rekey <rekey@me.com>
2022-10-26 01:53:38 +0800

    save

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 go mod tidy
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./cmd/app /dist/runner
+mkdir -p ./dist
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./cmd/app ./dist/runner
 docker build -t rekey/doh .
