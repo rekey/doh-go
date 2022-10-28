c3bf0cd6cabb23ffcbbff9a42315205f56b24613
Rekey <rekey@me.com>
2022-10-29 00:10:43 +0800

    save

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ mkdir -p ./dist
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./dist/web cmd/web.go
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./dist/tls cmd/tls.go
 docker build -t rekey/doh .
+docker build -t rekey/doh:tls ./tls.Dockerfile
--- cmd/tls.go
+++ cmd/tls.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"doh-go/app"
+	"flag"
 	"github.com/kabukky/httpscerts"
 	"log"
 	"net/http"
+	"os"
 	"path"
 )
 
 func main() {
+	domain := flag.String("domain", "", "dns.local")
+	flag.Parse()
+	ssl := *domain
+	if os.Getenv("ssl") != "" {
+		ssl = os.Getenv("ssl")
+	}
 	certPath := path.Join(app.StoreDir, "cert.pem")
 	keyPath := path.Join(app.StoreDir, "key.pem")
 	// Check if the cert files are available.
@@ -16,7 +24,7 @@ func main() {
 	log.Println(err)
 	// If they are not available, generate new ones.
 	if err != nil {
-		err = httpscerts.Generate(certPath, keyPath, "127.0.0.1")
+		err = httpscerts.Generate(certPath, keyPath, ssl)
 		if err != nil {
 			log.Fatal("Error: Couldn't create https certs.")
 		}
