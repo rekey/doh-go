973bccabfa835dba1ddfb7f353529b227cb27a28
Rekey <rekey@me.com>
2023-03-13 15:37:31 +0800

    save

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 go mod tidy
 mkdir -p ./dist
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./docker/web/web cmd/web.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./docker/tls/tls cmd/tls.go
+CGO_ENABLED=0 
+GOOS=linux
+GOARCH=amd64
+go build -o ./docker/web/web cmd/web.go
+go build -o ./docker/tls/tls cmd/tls.go
 docker build -t rekey/doh docker/web
 docker build -t rekey/doh:tls docker/tls
